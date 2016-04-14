80e69ba9741e0dadad45d6fb2f993470346d64e6
Bai <412054619@qq.com>
2016-04-13 17:54:02 -0700

    remote push

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+'''
+Created on 2016年4月12日
+
+@author: PI
+'''
+import urllib2
+
+
+class HtmlDownloader(object):
+    
+    
+    def download(self,url):
+        if url is None:
+            return None
+        
+        response = urllib2.urlopen(url)
+        
+        if response.getcode() !=200:
+            return None
+        return response.read()
+    
+    
+
+
+
